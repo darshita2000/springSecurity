990a1af4eb8840435f941b1268634cf4363199cf
Darshita <108191705+darshita2000@users.noreply.github.com>
2024-04-18 11:24:02 +0530

    first commit

---
+++ src/main/java/com/example/springsecurity/controller/GreetingsController.java
@@ -0,0 +1,20 @@
+package com.example.springsecurity.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/greetings")
+public class GreetingsController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return  ResponseEntity.ok("Hello from our api");
+    }
+    @GetMapping("/say-good-bye")
+    public ResponseEntity<String> saygoodBye(){
+        return  ResponseEntity.ok("Good bye and see you later");
+    }
+
+}
